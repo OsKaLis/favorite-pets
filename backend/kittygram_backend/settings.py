bdea5204c5e7b6110a66cd7240f2b49161b57d69
Юра Юшко <oskaLis@yandex.ru>
2023-09-07 19:53:17 +0300

    correction full v1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,11 +2,15 @@
 import os
 from pathlib import Path
 
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -56,8 +60,6 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
