1aaf4f3266a01c8b5d16d770abeccdca51970995
Юра Юшко <oskaLis@yandex.ru>
2023-09-09 21:23:35 +0300

    correction rabota s media v4.3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,10 +1,11 @@
 # flake8: noqa
 import os
+import uuid
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', uuid.uuid4()) 
 
 DEBUG = os.getenv('DEBUG', bool(0)) == 'True'
 
